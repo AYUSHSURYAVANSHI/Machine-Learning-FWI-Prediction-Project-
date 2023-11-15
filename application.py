4ce0b6008a3d64fe32485d6343bcff3ea9cf9210
AYUSHSURYAVANSHI <surywanshiayush@gmail.com>
2023-11-15 23:25:44 +0530

    first commit

--- application.py
+++ application.py
@@ -1,14 +1,17 @@
 import pickle
-
 from flask import Flask,request,jsonify,render_template
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
 from sklearn.preprocessing import StandardScaler
-app = Flask(__name__)
-#pickle file 
+
+application = Flask(__name__)
+app=application
+
+## import ridge regresor model and standard scaler pickle
 ridge_model=pickle.load(open('models/ridge.pkl','rb'))
 standard_scaler=pickle.load(open('models/scaler.pkl','rb'))
 
+## Route for home page
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -16,17 +19,16 @@ def index():
 @app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='POST':
-        pass
         Temperature=float(request.form.get('Temperature'))
-        RH=float(request.form.get('RH'))
-        Ws=float(request.form.get('Ws'))
-        Rain=float(request.form.get('Rain'))
-        FFMC=float(request.form.get('FFMC'))
-        DMC=float(request.form.get('DMC'))
-        ISI=float(request.form.get('ISI'))
-        Classes=float(request.form.get('Classes'))
-        Region=float(request.form.get('Region'))
-        # Temperature=float(request.form.get('Temperature'))
+        RH = float(request.form.get('RH'))
+        Ws = float(request.form.get('Ws'))
+        Rain = float(request.form.get('Rain'))
+        FFMC = float(request.form.get('FFMC'))
+        DMC = float(request.form.get('DMC'))
+        ISI = float(request.form.get('ISI'))
+        Classes = float(request.form.get('Classes'))
+        Region = float(request.form.get('Region'))
+
         new_data_scaled=standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
         result=ridge_model.predict(new_data_scaled)
 
@@ -35,9 +37,6 @@ def predict_datapoint():
     else:
         return render_template('home.html')
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5500)
+    app.run(host="0.0.0.0")
